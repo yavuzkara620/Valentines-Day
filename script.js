16bdcd1523c4171fa87e70051b9241029488ce37
yavuzkara620 <ahmetyavuz1718@gmail.com>
2026-02-12 00:19:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -91,13 +91,13 @@ function drawText() {
 
     if(frameNumber < 250){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        context.fillText("her gün ne kadar şanslı olduğuma inanamıyorum.", canvas.width/2, canvas.height/2);
         opacity = opacity + 0.01;
     }
     //fades out the text by decreasing the opacity
     if(frameNumber >= 250 && frameNumber < 500){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        context.fillText("her gün ne kadar şanslı olduğuma inanamıyorum.", canvas.width/2, canvas.height/2);
         opacity = opacity - 0.01;
     }
 
