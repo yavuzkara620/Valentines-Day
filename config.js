ec7bd9e3e10a0caf34645d2355257bc4b51bc51f
yavuzkara620 <ahmetyavuz1718@gmail.com>
2026-02-12 01:12:18 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  music: {
+        enabled: true,                     // Music feature is enabled
+        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
+        musicUrl: "https://res.cloudinary.com/dalc73sbr/video/upload/v1770759481/I_Wanna_Be_Yours_mt2cmn.mp3", // Music streaming URL
+        startText: "🎵 Play Music",        // Button text to start music
+        stopText: "🔇 Stop Music",         // Button text to stop music
+        volume: 0.5                        // Volume level (0.0 to 1.0)
+    }
+};
